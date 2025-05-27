fe7681d7c4adc03116ac93009cd9bc1b5511e27b
tw1996-dev <19tomaszwojcik96@gmail.com>
2025-05-27 09:47:31 +0200

    login section
    login section

---
+++ scripts/login-form.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginBtn = document.querySelector('#login-btn');
+    const loginFormContainer = document.querySelector('#loginForm');
+    const formCloseBtn = document.querySelector('#form-close');
+
+    loginBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        loginFormContainer.classList.add('active');
+    });
+
+    formCloseBtn.addEventListener('click', () => {
+        loginFormContainer.classList.remove('active');
+    });
+
+    loginFormContainer.addEventListener('click', (event) => {
+        if (event.target === loginFormContainer) {
+            loginFormContainer.classList.remove('active');
+        }
+    });
+});
